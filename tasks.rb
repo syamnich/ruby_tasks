456e8781b9ff10ce2709aa8e5d4f8dd73d50aadc
syamnich <sergey.yamnich@gmail.com>
2017-01-02 22:25:13 +0200

    5,21,25,69,sort array, local_max, reduce

--- tasks.rb
+++ tasks.rb
@@ -6,13 +6,10 @@ end
 
 # 5. Дан целочисленный массив. Преобразовать его, прибавив к четным числам первый элемент. 
 # Первый и последний элементы массива не изменять.
-# def task5
-#   p a = [4, 2, 5, 4, 3, 6, 9, 8]
-#   first = a.first
-#   p a[1...a.size-1].map! { |i| i.even? ? i + first : i }
-#   p a
-# end
-
+def task5
+  p a = [4, 2, 5, 4, 3, 6, 9, 8]
+  p a.each_with_index { |v, i| a[i] = v + a.first if v.even? && i !=0 && i != a.size-1 }
+end
 
 # 9. Дан целочисленный массив. Заменить все положительные элементы на значение минимального.
 def task9
@@ -33,15 +30,26 @@ def task17
 end
 
 # 21. Дан целочисленный массив. Определить количество участков, на которых его элементы монотонно возрастают.
-# def task21
-#   a = [2, 3, 5, 6, 7, -2, 4, 1, 2, 3]
-# end
+def task21
+  a = [2, 3, 5, 10, 10, 2, 4]
+  count = 0
+  a[1..a.size-2].each.with_index(1) { |v, i| count += 1 if (a[i] > a[i+1] && a[i] >= a[i-1]) || (a[i] <= a[i+1] && i == a.size-2) }
+  p count
+end
 
 # 25. Дан целочисленный массив. Преобразовать его, вставив перед каждым положительным элементом нулевой элемент.
-# def task25
-#   p a = [-2, 3, -5, 4]
-#   p a.each_with_object([]) { |i, acc| acc << a.first if i > 0; acc << i; acc }
-# end
+def task25
+  p a = [1, 3, -5, 4, 2, -5, 6]
+  i = 0
+  a[0..a.size-1].each do |v| 
+    if v > 0
+      a.insert(i, a.first) 
+      i += 1
+    end
+    i += 1
+  end
+  p a
+end
 
 # 29. Дан целочисленный массив. Упорядочить его по возрастанию
 def task29
@@ -90,8 +98,7 @@ end
 # 53. Дан целочисленный массив. Найти количество элементов, расположенных перед последним максимальным.
 def task53
   p a = [2, 7, -4, 7, 3, 4]
-  i = a.reverse.index(a.max)
-  p a.size - 1 - i 
+  p a.rindex(a.max)
 end
 
 # 57. Дан целочисленный массив. Найти индекс первого экстремального (то есть минимального или 
@@ -115,17 +122,13 @@ end
 
 # 69. Дано вещественное число R и массив вещественных чисел. 
 # Найти два элемента массива, сумма которых наиболее близка к данному числу.
-# def task69
-#   p r = 10.2
-#   p a = [1.15, 3.6, 8.4, 5.55]
-#   min = []
-#   a[0..a.size-2].each_with_index do |v,i| 
-#     sum = a[0] + a[i + 1] 
-#     min << (r - sum).abs
-#   end
-#   p min
-#   p min.min
-# end
+def task69
+  p r = 10.2
+  p a = [1.15, 3.6, 8.4, 5.55]
+  comb = a.combination(2).to_a
+  min = comb.map.with_index { |v, i| (v.first + v.last - r).abs }.min
+  p comb.select { |i| (i.first + i.last - r).abs == min }.flatten
+end
 
 # 73. Дан целочисленный массив. Удалить все элементы, встречающиеся ровно два раза.
 def task73
@@ -165,8 +168,8 @@ end
 # Если все элементы массива меньше К, то вывести true; в противном случае вывести false.
 def task93
   p a = [1, 3, 8, 5, 4, 2, 0]
-  p k = 0
-  a.select { |i| i > k }.count == 0 ? p(true) : p(false)
+  p k = 10
+  p a.select { |i| i > k }.count == 0 ? true : false
 end
 
 # 97. Дан целочисленный массив и число К. Вывести индекс последнего элемента, меньшего К.
@@ -185,6 +188,13 @@ def task101
   p a[1..a.size-1].select.with_index{ |v, i| p i+1 if a[i+1] > a[i] }.count
 end
 
+# 105. Дан целочисленный массив. Если данный массив образует убывающую последовательность, то вывести nil, 
+# в противном случае вывести номер первого числа, нарушающего закономерность.
+def task105
+  p a = [9, 8, 17, 6, 3, 5, 2, 1]
+  p a[0..a.size-2].map.with_index { |v, i| i+1 if v < a[i+1] }.compact.first
+end
+
 # 109. Дан целочисленный массив. Найти минимальный и максимальный элемент в массиве.
 def task109
   p a = [1, 3, 8, 15, 4, 2, 3]
@@ -194,11 +204,43 @@ def task109
 end
 
 
-# не решены 5, 21, 25, 69
+# Сортировка массива: чет/нечет
+def srt
+  p a = [1, 3, 8, 15, 4, 2, 3]
+  p a.partition { |i| i.even? }.map { |array| array.sort }.flatten
+end
 
+# Сортировка массива: обратный порядок
+def rvr
+  p a = [1, 3, 8, 15, 4, 2, 3]
+  i = 0
+  while i < (a.size)/2 do
+    t = a[i]
+    a[i] = a[a.size - 1 - i]
+    a[a.size - 1 - i] = t
+    i += 1
+  end
+  p a
+end
 
-task101
-
+# Сортировка массива: массив строк упорядочить по длине слов (группировка по длине слов)
+def sort_by_length
+  p a = ['three', 'four', 'one', 'si']
+  i = 0
+  while i < a.size do
+    j = i + 1
+    while j < a.size do
+      if a[i].size > a[j].size
+        t = a[i]
+        a[i] = a[j]
+        a[j] = t
+      end
+      j += 1
+    end
+    i += 1
+  end
+  p a
+end
 
 # Поиск в массиве: локальные максимумы
 def local_max
@@ -212,4 +254,47 @@ def local_max
     i += 1
   end
   p a; p c; p c.count
+end
+
+# map через reduce
+def new_map(array)
+  array.reduce([]) { |acc, el| acc << yield(el) }
+end
+
+# select через reduce
+def new_select(array)
+  array.reduce([]) { |acc, el| yield(el) ? acc << el : acc }
+end
+
+# detect через reduce
+def new_detect(array)
+  array.reduce { |acc, el| return el if yield(el)}
+end
+
+# count через reduce
+def new_count(array, item = nil)
+  if block_given?
+    array.reduce([]) { |acc, el|  yield(el) ? acc << el : acc }.size
+  elsif item
+    array.select { |i| i == item }.size
+  else
+    array.size
+  end
+end
+
+# all? через reduce
+def new_all?(array)
+  if block_given?
+    array.reduce(true) { |acc, el| return false if !yield(el); acc }
+  else
+    array.reduce(true) { |acc, el| return false if el == nil || el == false; acc }
+  end
+end
+
+a = [nil, 3, 3, 7, 6]
+p new_all?(a) #{ |i| i > 5 }
+
+# flatten
+def new_flatten
+  p a = [[1, 2, 3], [4, 5, 6], [7, [8, 9]]]
 end
